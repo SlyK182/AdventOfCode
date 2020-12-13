f40cbae70810b28566266110061dd55bc0c733d3
ComradeSlyK <gregorini.silvio@gmail.com>
2020-12-13 22:45:42 +0100

    Updated load_input function to read test files if necessary

--- input_loader.py
+++ input_loader.py
@@ -1,5 +1,7 @@
 # Author: ComradeSlyK (gregorini.silvio@gmail.com)
 
+import sys
+
 from os import path
 from pathlib import Path
 
@@ -7,5 +9,9 @@ from pathlib import Path
 def load_input(day, num):
     curdir = str(Path(path.abspath(__file__)).parent)
     day = str(day).zfill(2)
-    with open(path.join(curdir, 'inputs', f'day{day}_{num}.txt'), 'r') as f:
+    filename = f'day{day}_{num}'
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'test':
+        filename += '_test'
+    filename += '.txt'
+    with open(path.join(curdir, 'inputs', filename), 'r') as f:
         return f.readlines()
