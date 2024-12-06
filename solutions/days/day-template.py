d2f1898cf6e16805087e6a6fc19c9a18afe93c00
SlyK182 <gregorini.silvio@gmail.com>
2024-12-06 19:24:15 +0100

    Template file for 'dayxx.py' files

---
+++ solutions/days/day-template.py
@@ -0,0 +1,22 @@
+from ..utils.common import load_input, timer
+
+
+@timer
+def problem1_solution():
+    from pathlib import Path
+    raise NotImplementedError(f"{Path(__file__).name[:-3]}.problem1_solution() not implemented yet")
+
+
+@timer
+def problem2_solution():
+    from pathlib import Path
+    raise NotImplementedError(f"{Path(__file__).name[:-3]}.problem2_solution() not implemented yet")
+
+
+def main():
+    print(f"** Solution to problem 1: {problem1_solution()} **")
+    print(f"** Solution to problem 2: {problem2_solution()} **")
+
+
+if __name__ == "__main__":
+    main()
