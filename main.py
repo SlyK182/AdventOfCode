3bbbfb3cda5d4b235ba94f29b219f699663f9834
SlyK182 <gregorini.silvio@gmail.com>
2024-12-06 23:38:32 +0100

    Allow running specific days' solutions only (instead of all)

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
+from solutions.days import main
+
+
+def _get_args():
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--days", type=lambda a: list(map(int, a.split(','))))
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    from solutions.days import main
-    main()
+    args = _get_args()
+    main(args.days or None)
--- solutions/days/__init__.py
+++ solutions/days/__init__.py
@@ -3,7 +3,14 @@ from pathlib import Path
 from re import match
 
 
-def main():
-    for fpath in sorted(Path(__file__).parent.iterdir(), key=lambda p: p.name):
-        if match(r"^day\d{2}\.py", fpath.name):
-            import_module(f".{fpath.name[:-3]}", package=__name__).main()
+def main(days: list[int] = None):
+    if days:
+        mods = [f".day{str(d).zfill(2)}" for d in days]
+    else:
+        mods = [
+            f".{p.name[:-3]}"
+            for p in sorted(Path(__file__).parent.iterdir(), key=lambda p: p.name)
+            if match(r"^day\d{2}\.py", p.name)
+        ]
+    for mod in mods:
+        import_module(mod, package=__name__).main()
