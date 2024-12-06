6e3b82ab42ae7f480f84e060cdeee735bcb99eac
SlyK182 <gregorini.silvio@gmail.com>
2024-12-06 19:22:24 +0100

    Solutions for day 3

--- solutions/days/day03.py
+++ solutions/days/day03.py
@@ -1,14 +1,39 @@
+import re
+
 from ..utils.common import load_input, timer
+
+DATA = "".join(load_input(3, 1))
 
 
 @timer
 def problem1_solution():
-    return "NONE"
+    return sum(
+        eval(v, {}, {'mul': int.__mul__})
+        for v in re.findall(r"mul\(\d{1,3},\d{1,3}\)", DATA)
+    )
 
 
 @timer
 def problem2_solution():
-    return "NONE"
+    to_sum = ""
+    to_check = str(DATA)
+    while to_check:
+        if to_check.startswith("don't()"):
+            if (do_idx := to_check.find("do()")) != -1:
+                to_check = to_check[do_idx:]
+            else:
+                to_check = ""
+        else:
+            if (dont_idx := to_check.find("don't()")) == -1:
+                to_sum += to_check
+                to_check = ""
+            else:
+                to_sum += to_check[:dont_idx]
+                to_check = to_check[dont_idx:]
+    return sum(
+        eval(v, {}, {'mul': int.__mul__})
+        for v in re.findall(r"mul\(\d{1,3},\d{1,3}\)", to_sum)
+    )
 
 
 def main():
